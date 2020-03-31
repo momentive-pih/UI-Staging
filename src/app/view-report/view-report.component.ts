aa809c8cc7923e4296cff0123b5140979f5b4689
Thamaraiselvan <818589@cognizant.com>
2020-03-31 12:10:11 +0530

    Self service Report

--- src/app/view-report/view-report.component.ts
+++ src/app/view-report/view-report.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Attribute } from '@angular/compiler';
-import {MatTableDataSource} from '@angular/material';
-import {TableModule} from 'primeng/table';
+import { MatTableDataSource } from '@angular/material';
+import { TableModule } from 'primeng/table';
 import * as frLocale from 'date-fns/locale/fr';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDatepickerInputEvent} from '@angular/material/datepicker';
-import {HomeService} from '../service/home-service.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { HomeService } from '../service/home-service.service';
 import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { NgSelectModule, NgOption} from '@ng-select/ng-select';
-import { MomentiveService} from '../service/momentive.service';
+import { NgSelectModule, NgOption } from '@ng-select/ng-select';
+import { MomentiveService } from '../service/momentive.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Ng2FilterPipeModule } from 'ng2-filter-pipe';
 // import * as jsPDF from 'jspdf';
@@ -29,7 +29,7 @@ declare var $: any;
 })
 export class ViewReportComponent implements OnInit {
 
-  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
+  @ViewChild('pdfTable', { static: false }) pdfTable: ElementRef;
 
   SelectAllcheckValue = false;
   userFilter: any = { Name: '' };
@@ -47,8 +47,8 @@ export class ViewReportComponent implements OnInit {
   searchRelatedMessage = false;
   // New Data;
   productdata: any = [];
-  productApplication:any =[];
-  thirdLevelData: any =[];
+  productApplication: any = [];
+  thirdLevelData: any = [];
   thirdlevelCheckvalue: any = [];
 
   // Report Code
@@ -56,13 +56,24 @@ export class ViewReportComponent implements OnInit {
   firstcCategories = false;
   secondCategories = false;
   thirdCategories = false;
-
+  componentReportPage = false;
+  thirdLevelCategories = true;
+  ReportViewCheck: any = [];
   viewReportId: any;
-  UserSelectedProducts: any =[];
+  UserSelectedProducts: any = [];
   objectKeys = Object.keys;
+
+  productAttributeReport: any = [];
+  productComplianceReport: any = [];
+  customerCommunicationReport: any = [];
+  toxicologyReport: any = [];
+  restrictedSubstanceReport: any = [];
+  salesInformationReport: any = [];
+  releaseReport: any = [];
+  subMenuCheck: any = [];
   constructor(private fb: FormBuilder, private route: ActivatedRoute,
-              private router: Router, private homeService: HomeService,
-              private momentiveService: MomentiveService) {
+    private router: Router, private homeService: HomeService,
+    private momentiveService: MomentiveService) {
 
 
   }
@@ -70,7 +81,7 @@ export class ViewReportComponent implements OnInit {
   ngOnInit() {
     this.viewReportId = localStorage.getItem('SearchBarData');
 
-    this.UserSelectedProducts =JSON.parse(localStorage.getItem('SearchBarData'));
+    this.UserSelectedProducts = JSON.parse(localStorage.getItem('SearchBarData'));
 
     if (this.UserSelectedProducts.length > 0) {
       this.noReportPage = true;
@@ -104,7 +115,7 @@ export class ViewReportComponent implements OnInit {
       { app_id: 13, app_text: 'Respiratory / anesthesia' },
       { app_id: 14, app_text: 'Medical equipment keypads' },
     ];
-    
+
 
   }
 
@@ -122,35 +133,35 @@ export class ViewReportComponent implements OnInit {
   }
   onItemSelect(item: any) {
     console.log(item);
-}
-OnItemDeSelect(item: any) {
+  }
+  OnItemDeSelect(item: any) {
     console.log(item);
-}
-onSelectAll(items: any) {
+  }
+  onSelectAll(items: any) {
     console.log(items);
-}
-onDeSelectAll(items: any) {
+  }
+  onDeSelectAll(items: any) {
     console.log(items);
-}
+  }
 
-intialSort() {
-  return 0;
-}
+  intialSort() {
+    return 0;
+  }
 
 
-public downloadAsPDF() {
+  public downloadAsPDF() {
 
-  const data = document.getElementById('pdfTable');
-  data.setAttribute('style','right:0; top:0; bottom:0;padding:20px;');
-html2canvas(data).then(canvas => {
-  var options = {
-    beforePageContent: header,
-    pagesplit: true,
-  };
+    const data = document.getElementById('pdfTable');
+    data.setAttribute('style', 'right:0; top:0; bottom:0;padding:20px;');
+    html2canvas(data).then(canvas => {
+      var options = {
+        beforePageContent: header,
+        pagesplit: true,
+      };
       var contentWidth = canvas.width;
       var contentHeight = canvas.height;
 
-    
+
 
       //The height of the canvas which one pdf page can show;
       var pageHeight = contentWidth / 592.28 * 841.89;
@@ -163,179 +174,229 @@ html2canvas(data).then(canvas => {
       var position = 0;
       //a4 format [595.28,841.89]	      
       var imgWidth = 595.28;
-      var imgHeight = 592.28/contentWidth * contentHeight;
+      var imgHeight = 592.28 / contentWidth * contentHeight;
 
       var pageData = canvas.toDataURL('image/jpeg', 1.0);
 
       var pdf = new jsPDF('p', 'pt', 'a4');
       // pdf.page = 1;
 
-      
-     var tbl_res = pdf.autoTableHtmlToJson(document.getElementById('mytable'));        
-     pdf.autoTable(tbl_res.columns, tbl_res.data, options);
-       pdf.internal.scaleFactor = 2.25;
+
+      var tbl_res = pdf.autoTableHtmlToJson(document.getElementById('mytable'));
+      pdf.autoTable(tbl_res.columns, tbl_res.data, options);
+      pdf.internal.scaleFactor = 2.25;
 
 
-     // let columns = ["ID", "Name", "Age", "City"];
+      // let columns = ["ID", "Name", "Age", "City"];
       // var data = [
       //     [1, "Jonatan", 25, "Gothenburg"],
-         
+
       // ];
 
-   
+
       var riskoptions = {
         tableWidth: 'auto',
         addPageContent: header,
-        margin: {  top: 5, horizontal: 7 },
+        margin: { top: 5, horizontal: 7 },
         // startY: pdf.autoTableEndPosY() + 40,
-        columnStyles: {0: {columnWidth: 'wrap'}}
-       
-    };
-      var header = function(data) {
+        columnStyles: { 0: { columnWidth: 'wrap' } }
+
+      };
+      var header = function (data) {
         pdf.setFontSize(18);
         pdf.setTextColor(40);
         pdf.setFontStyle('normal');
         //doc.addImage(headerImgData, 'JPEG', data.settings.margin.left, 20, 50, 50);
         // pdf.text("Testing Report", pdf.settings.margin.left, 50);
       };
-    
-    //   function footer() { 
-    //     pdf.text(150,285, 'page ' + pdf.page); //print number bottom right
-    //     pdf.page ++;
-    // };
+
+      //   function footer() { 
+      //     pdf.text(150,285, 'page ' + pdf.page); //print number bottom right
+      //     pdf.page ++;
+      // };
 
       // pdf.internal.getVerticalCoordinateString(y),"","Td"
 
-     if (leftHeight < pageHeight) {
-          pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+      if (leftHeight < pageHeight) {
+        pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight);
       } else {
-          while(leftHeight > 0) {
-              pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
-              leftHeight -= pageHeight;
-              position -= 841.89;
-              //avoid blank page
-              if(leftHeight > 0) {
-                pdf.addPage();  
-                   
-              }
+        while (leftHeight > 0) {
+          pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+          leftHeight -= pageHeight;
+          position -= 841.89;
+          //avoid blank page
+          if (leftHeight > 0) {
+            pdf.addPage();
+
           }
+        }
       }
-  
-      pdf.save('Momentive-' + new Date() + '.pdf'); 
- 
-  })
-    }
+      pdf.save('Momentive-' + new Date() + '.pdf');
+    })
+  }
 
 
-SelectAllCategories(checkStatus) {
-  console.log(checkStatus);
-  console.log(this.sidebarCategoriesData);
-  if(checkStatus) {
-    this.sidebarCategoriesData.forEach(element => {element.checkValue = true;
-if(element.tab_content.length > 0) {
-  element.tab_content.forEach(element => {element.checkValue = true;
-this.viewReportPage = true; 
-this.noReportPage = false;
-});
-}
-    });
-  } else {
-    this.sidebarCategoriesData.forEach(element => {element.checkValue = false;
-      if(element.tab_content.length > 0) {
-        element.tab_content.forEach(element => {element.checkValue = false;
-      });
-
-      this.viewReportPage = false; 
-       this.noReportPage = true;  
-      }
+  SelectAllCategories(checkStatus) {
+    console.log(checkStatus);
+    console.log(this.sidebarCategoriesData);
+    this.ReportViewCheck = this.sidebarCategoriesData;
+    console.log(this.ReportViewCheck);
+    this.reportView();
+    if (checkStatus) {
+      this.sidebarCategoriesData.forEach(element => {
+        element.checkValue = true;
+        if (element.tab_content.length > 0) {
+          element.tab_content.forEach(element => {
+            element.checkValue = true;
+            this.componentReportPage = true;
+            if (element.sub.length > 0) {
+              element.sub.forEach(childelement => {
+                childelement.checkValue = true;
+                this.componentReportPage = true;
+              });
+            }
+            this.viewReportPage = true;
+            this.noReportPage = false;
           });
-} 
-}
-
-topCheckBox(checkStatus, data) {
-  alert('1')
-  console.log(checkStatus)
- if (checkStatus === true) {
-    data.tab_content.forEach(element => {element.checkValue = true;
-         if (element.sub.length > 0) {
-      element.sub.forEach(childelement => {childelement.checkValue = true;
-    });
-  }
-    });
-  } else {
-    alert('2');
-    this.SelectAllcheckValue = false;
-    data.tab_content.forEach(element => {element.checkValue = false;
-          if (element.sub.length > 0) {
-              element.sub.forEach(childelement => {childelement.checkValue = false;
+        }
+      });
+    } else {
+      this.sidebarCategoriesData.forEach(element => {
+        element.checkValue = false;
+        this.componentReportPage = false;
+        if (element.tab_content.length > 0) {
+          element.tab_content.forEach(element => {
+            element.checkValue = false;
+            this.componentReportPage = false;
+            if (element.sub.length > 0) {
+              element.sub.forEach(childelement => {
+                childelement.checkValue = false;
+                this.componentReportPage = false;
+              });
+            }
+          });
+          this.viewReportPage = false;
+          this.noReportPage = true;
+        }
       });
     }
+  }
+
+  topCheckBox(checkStatus, data) {
+    alert('0')
+    this.ReportViewCheck = this.sidebarCategoriesData;
+    console.log(this.ReportViewCheck);
+    this.reportView();
+    console.log(checkStatus)
+    console.log(data);
+    if (checkStatus === true) {
+
+      data.tab_content.forEach(element => {
+        element.checkValue = true;
+        if (data.tab_content.length === 7) {
+          alert('1');
+          this.componentReportPage = true;
+        }
+        if (element.sub.length > 0) {
+          element.sub.forEach(childelement => {
+            childelement.checkValue = true;
+            // this.SelectAllcheckValue = true;
+            this.componentReportPage = true;
+          });
+        }
+      });
+    } else {
+      data.tab_content.forEach(element => {
+        element.checkValue = false;
+        this.componentReportPage = false;
+        if (data.tab_content.length != 7) {
+          this.SelectAllcheckValue = false;
+          this.componentReportPage = false;
+        }
+        if (element.sub.length > 0) {
+          this.SelectAllcheckValue = false;
+          element.sub.forEach(childelement => {
+            childelement.checkValue = false;
+            this.componentReportPage = false;
+          });
+        }
+      });
+    }             
+    this.ReportViewCheck.some(element => { element != element.checkValue;
+      let menuCheck = element.checkValue;
+      console.log(menuCheck);
+      if(menuCheck === true ) {
+        this.SelectAllcheckValue = true;
+      } else {
+        this.SelectAllcheckValue = false;
+      }
   });
-}
-}
-subCheckBox(checkStatus,data,id,top_id) {
+  }
 
-  console.log(top_id);
-  console.log(id);
-  console.log(checkStatus);
-  console.log(data);
-  console.log(this.sidebarCategoriesData);
-  this.parentObject = this.sidebarCategoriesData[id];
-  console.log(this.parentObject);
-  if (checkStatus) {
-      data.checkValue = true;
-      let top_id ;
-      this.parentObject.tab_content.forEach(element => {
-        alert('i')
-     if (element.checkValue) {
-       element.sub.forEach(childelement => { childelement.checkValue = true;
-       });
-       top_id++;
-     }
-     console.log(top_id, this.parentObject.tab_content.length);
-     if (top_id === this.parentObject.tab_content.length) {
-      alert('iuuu')
-      this.parentObject.checkValue = true;
-    }
-     console.log(this.parentObject);
-   });
-   } else {
-
-    this.SelectAllcheckValue = false;
-    alert('777');
-    this.parentObject.tab_content.forEach(element => {
-    if (element.checkValue) {
-      element.sub.forEach(childelement => { childelement.checkValue = false;
+  subCheckBox(checkStatus, data, id, product_Name, top_id) {
+    this.ReportViewCheck = this.sidebarCategoriesData;
+    console.log(this.ReportViewCheck);
+    this.reportView();
+    console.log(data);
+    this.parentObject = this.sidebarCategoriesData[top_id].tab_content[id];
+    if (checkStatus === false) {
+      this.sidebarCategoriesData.forEach(element => {
+      element.Name = product_Name;
+        this.sidebarCategoriesData[top_id].checkValue = false;
+        this.SelectAllcheckValue = false;
+      });
+    } else {
+      this.sidebarCategoriesData.forEach(element => {
+      element.Name = product_Name;
+        console.log(this.sidebarCategoriesData);
+        this.subMenuCheck = this.sidebarCategoriesData[top_id].tab_content;
+        console.log(this.subMenuCheck);
+        this.subMenuCheck.every(element => { element.checkValue === true;
+          let menuCheck = element.checkValue;
+          if(menuCheck === true ) {
+            this.sidebarCategoriesData[top_id].checkValue = true;
+            this.SelectAllcheckValue = true;
+          } else {
+            this.sidebarCategoriesData[top_id].checkValue = false;
+            this.SelectAllcheckValue = false;
+          }
+         
+        });
       });
     }
-  });
 
-    data.checkValue = false;
-    this.parentObject.checkValue = false;
-    data.sub.forEach(element => { element.checkValue = false;
-    });
-   }
-}
-
-
-nextsubCheckBox(checkStatus, data, id) {
-  console.log(checkStatus);
-console.log(id);
-  this.parentObject = this.sidebarCategoriesData[id];
-  console.log(this.parentObject);
-  if (checkStatus) {
-    this.pdfIcon = true;
-    this.parentObject.tab_content[id].checkValue = true;
-  } else if (this.parentObject.tab_content[id].name) {
-      console.log(this.parentObject.tab_content[id].name);
-      this.SelectAllcheckValue = false;
-      this.parentObject.checkValue = false;
-      this.parentObject.tab_content[id].checkValue = false;
-  } else {
-    this.SelectAllcheckValue = true;
-    this.parentObject.checkValue = true;
-    this.parentObject.tab_content[id].checkValue = true; 
   }
+
+  reportView() {
+    this.productAttributeReport = this.ReportViewCheck[0];
+    this.productComplianceReport = this.ReportViewCheck[1];
+    this.customerCommunicationReport = this.ReportViewCheck[2];
+    this.toxicologyReport = this.ReportViewCheck[3];
+    this.restrictedSubstanceReport = this.ReportViewCheck[4];
+    this.salesInformationReport = this.ReportViewCheck[5];
+    this.releaseReport = this.ReportViewCheck[6];
   }
+
+  // nextsubCheckBox(checkStatus, data, id) {
+  //   console.log(checkStatus);
+  //   console.log(id);
+  //   this.parentObject = this.sidebarCategoriesData[id];
+  //   console.log(this.parentObject);
+  //   if (checkStatus) {
+  //     this.pdfIcon = true;
+  //     this.parentObject.tab_content[id].checkValue = true;
+  //   } else if (this.parentObject.tab_content[id].name) {
+  //     console.log(this.parentObject.tab_content[id].name);
+  //     this.SelectAllcheckValue = false;
+  //     this.parentObject.checkValue = false;
+  //     this.parentObject.tab_content[id].checkValue = false;
+  //     this.componentReportPage = false;
+  //   } else {
+  //     this.SelectAllcheckValue = true;
+  //     this.parentObject.checkValue = true;
+  //     this.parentObject.tab_content[id].checkValue = true;
+  //     this.componentReportPage = true;
+  //   }
+  // }
 
 }
